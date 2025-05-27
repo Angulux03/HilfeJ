7f5857942981b3616a4123d7f6c9b0328f6ea43e
Tom Stegemann <tom.l.stegemann@gmx.de>
2025-05-27 16:25:44 +0200

    Add files via upload

---
+++ mit unentschieden/Main.java
@@ -0,0 +1,30 @@
+
+/**
+ * Main Klasse /Startet spiel
+ */
+public class Main
+{
+    // instance variables - replace the example below with your own
+    Spiel spiel;
+
+    /**
+     * Constructor for objects of class Main
+     */
+    public Main()
+    {
+        // initialise instance variables
+        spiel = new Spiel();
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void Start()
+    {
+        // put your code here
+        spiel.spiel();
+    }
+}
---
+++ mit unentschieden/Runde.java
@@ -0,0 +1,118 @@
+import java.util.HashMap;
+
+/**
+ * Durchführen einer Runde
+ * 
+ * @author Philip
+ * @version V3 2025
+ */
+public class Runde {
+    private HashMap<Integer, Integer> spielfeld;
+    private Computer computer;
+    private Spielerin spielerin;
+    private SpielfeldAusgabe spielfeldAusgabe;
+    private int beginner;
+
+    public Runde(Computer computer) {
+        this.computer = computer;
+        this.spielfeld = new HashMap<>();
+        this.spielfeldAusgabe = new SpielfeldAusgabe(spielfeld);
+        this.spielerin = new Spielerin(spielfeldAusgabe);
+        this.beginner = 1;
+    }
+
+    public int starteRunde() {
+    vorbereiten();
+
+    while (true) {
+        if (beginner == 1) {
+            if (spielerinnenZug()) return 1;
+            if (spielfeld.size() == 9) {
+                System.out.println("Alle Felder sind belegt. Unentschieden.");
+                wechsleBeginner();
+                return 0;
+            }
+
+            if (computerZug()) return 2;
+            if (spielfeld.size() == 9) {
+                System.out.println("Alle Felder sind belegt. Unentschieden.");
+                wechsleBeginner();
+                return 0;
+            }
+        } else {
+            if (computerZug()) return 2;
+            if (spielfeld.size() == 9) {
+                System.out.println("Alle Felder sind belegt. Unentschieden.");
+                wechsleBeginner();
+                return 0;
+            }
+
+            if (spielerinnenZug()) return 1;
+            if (spielfeld.size() == 9) {
+                System.out.println("Alle Felder sind belegt. Unentschieden.");
+                wechsleBeginner();
+                return 0;
+            }
+        }
+    }
+    }
+    private void vorbereiten() {
+        spielfeld.clear();
+        spielfeldAusgabe.leeresSpielfeld();
+    }
+
+    private boolean spielerinnenZug() {
+    int feld = spielerin.gueltigeEingabe(spielfeld);
+    spielfeld.put(feld, 1);
+    spielfeldAusgabe.ausgabeSpielfeld();
+
+    if (rundeAuswerten(1)) {
+        System.out.println("Sie haben diese Runde gewonnen!");
+        wechsleBeginner();
+        return true;
+    }
+    
+    return false;
+    }
+
+    private boolean computerZug() {
+    int feld = computer.eingabeDesComputers(spielfeld);
+    spielfeld.put(feld, 2);
+    System.out.println("Der Computer hat Feld " + feld + " gewählt.");
+    spielfeldAusgabe.ausgabeSpielfeld();
+
+    if (rundeAuswerten(2)) {
+        System.out.println("Der Computer hat diese Runde gewonnen!");
+        wechsleBeginner();
+        return true;
+    }
+
+    return false;
+    }
+    
+
+    private void wechsleBeginner() {
+        if (beginner == 1) {
+            beginner = 2;
+        } else {
+            beginner = 1;
+        }
+    }
+
+    private boolean rundeAuswerten(int spieler) {
+        int[][] gewinnMöglichkeiten = {
+            {1, 2, 3}, {4, 5, 6}, {7, 8, 9},
+            {1, 4, 7}, {2, 5, 8}, {3, 6, 9},
+            {1, 5, 9}, {3, 5, 7}
+        };
+
+        for (int[] kombi : gewinnMöglichkeiten) {
+            if (spielfeld.getOrDefault(kombi[0], 0) == spieler &&
+                spielfeld.getOrDefault(kombi[1], 0) == spieler &&
+                spielfeld.getOrDefault(kombi[2], 0) == spieler) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
